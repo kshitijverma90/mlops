d25180e8dce118aad94cc5cbebb98e1f1db833b3
unknown <vkshitij471@gmail.com>
2023-06-24 23:53:17 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","")for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+        
+
+setup(
+  name='mlops',
+  version='0.0.1',
+  author='kshitij',
+  author_email='vkshitij471@gmail.com',
+  packages=find_packages(),
+  install_requires=get_requirements('requirements.txt')
+)
