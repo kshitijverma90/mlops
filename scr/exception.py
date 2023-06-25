47883eb91ebdd8b9be7c44415dcd756a9591afb2
KSHITIJ VERMA <80827279+kshitijverma90@users.noreply.github.com>
2023-06-26 01:06:41 +0530

    EDA AND MODEL TRAINING

--- scr/exception.py
+++ scr/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,10 +9,11 @@ def error_message_detail(error,error_detail:sys):
     return error_message 
     
 
-class CustomExceptioon(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detial=error_detail)
     def __str__(self):
         return self.error_message        
 
+  
